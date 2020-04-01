61b4cc4773d2f1441be07e9b19ba431f2e7a811b
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2020-04-01 12:08:17 +0300

    Finalize deployment script

--- 2020-03-remote-deployment/deploy/deploy.rb
+++ 2020-03-remote-deployment/deploy/deploy.rb
@@ -16,25 +16,31 @@ class Deploy
   def deploy(host, user, password)
     Net::SSH.start(host, user, password: password) do |connection|
       @connection = connection
-      # install_ruby
+      @scp = connection.scp
+      install_ruby
       copy_application_files
       install_required_gems(APP_DIR)
       create_app_user(APP_USER, APP_DIR)
-      # setup_systemd_service(APP_DIR)
-      # enable_systemd_service
+      setup_systemd_service(APP_DIR)
+      enable_systemd_service
+      restart_systemd_service
     end
   end
 
   def install_ruby
     checked_run('sudo', 'apt-get', 'update')
     checked_run('sudo', 'apt-get', 'install', '-y', 'build-essential')
-    archive_path = '/tmp/ruby-install-0.7.0.tar.gz'
-    checked_run('wget', '-O', archive_path, 'https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz')
-    checked_run('tar', '-C', '/tmp','-xzvf', archive_path)
-    ruby_install_dir = '/tmp/ruby-install-0.7.0'
-    checked_run('sudo', 'make', 'install', dir: ruby_install_dir)
+    unless valid_command?('which', 'ruby-install')
+      archive_path = '/tmp/ruby-install-0.7.0.tar.gz'
+      checked_run('wget', '-O', archive_path,
+        'https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz')
+      checked_run('tar', '-C', '/tmp','-xzvf', archive_path)
+      ruby_install_dir = '/tmp/ruby-install-0.7.0'
+      checked_run('sudo', 'make', 'install', dir: ruby_install_dir)
+    end
     checked_run('sudo', 'ruby-install', '-L')
-    checked_run('sudo', 'ruby-install', '--jobs=4', 'ruby', REQUIRED_RUBY_VERSION)
+    checked_run('sudo', 'ruby-install', '--no-reinstall', '--jobs=4', 'ruby',
+      REQUIRED_RUBY_VERSION)
   end
 
   def checked_run(*args, dir: nil)
@@ -63,8 +69,7 @@ class Deploy
     temp_dir = '/tmp/temp-app-dir'
     checked_run('sudo', 'rm', '-rf', temp_dir)
     puts 'Uploading application files'
-    scp = @connection.scp
-    scp.upload!(File.expand_path('..', __dir__), temp_dir, recursive: true)
+    @scp.upload!(File.expand_path('..', __dir__), temp_dir, recursive: true)
     checked_run('sudo', 'mkdir', '-p', APP_DIR)
     checked_run('sudo', 'cp', '-R', File.join(temp_dir, '*'), APP_DIR)
     checked_run('sudo', 'rm', '-rf', temp_dir)
@@ -92,20 +97,34 @@ class Deploy
 
   def setup_systemd_service(application_directory)
     template = File.read(File.expand_path('application.service.erb', __dir__))
-    path = ENV['PATH']
+    path = [
+      ruby_installation_path,
+      '/usr/local/bin',
+      '/usr/bin',
+      '/bin',
+    ].join(':')
     bundle_path = File.join(ruby_installation_path, 'bundle')
     clojure = binding
     baked_template = ERB.new(template).result(clojure)
 
-    file_path = File.join(__dir__, "#{SERVICE_NAME}.service")
+    service_file_name = "#{SERVICE_NAME}.service"
+    file_path = File.join(__dir__, service_file_name)
     File.write(file_path, baked_template)
 
-    checked_run('sudo', 'mv', file_path, '/etc/systemd/system')
+    remote_service_file = '/tmp/systemd.service'
+    @scp.upload(file_path, remote_service_file)
+    
+    checked_run('sudo', 'mv', remote_service_file, 
+      File.join('/etc/systemd/system', "#{SERVICE_NAME}.service"))
     checked_run('sudo', 'systemctl', 'daemon-reload')
   end
 
   def enable_systemd_service
     checked_run('sudo', 'systemctl', 'enable', SERVICE_NAME)
+  end
+
+  def restart_systemd_service
+    checked_run('sudo', 'systemctl', 'restart', SERVICE_NAME)
   end
 end
 
