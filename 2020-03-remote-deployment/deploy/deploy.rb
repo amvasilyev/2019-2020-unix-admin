dd1430b73cab99407cc0017f143e97313c8ee36a
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2020-04-06 13:28:47 +0300

    Reformat deploy code with rufo

--- 2020-03-remote-deployment/deploy/deploy.rb
+++ 2020-03-remote-deployment/deploy/deploy.rb
@@ -1,16 +1,15 @@
 #!/usr/bin/env ruby
 
-require 'net/scp'
-require 'net/ssh'
+require "net/scp"
+require "net/ssh"
 
-require 'erb'
-require 'tmpdir'
+require "erb"
+require "tmpdir"
 
-REQUIRED_RUBY_VERSION='2.6.5'
-APP_DIR = File.expand_path('/srv/roda-app')
-SERVICE_NAME = 'application'
-APP_USER = 'roda-app'
-
+REQUIRED_RUBY_VERSION = "2.6.5"
+APP_DIR = File.expand_path("/srv/roda-app")
+SERVICE_NAME = "application"
+APP_USER = "roda-app"
 
 class Deploy
   def deploy(host, user, password)
@@ -28,23 +27,23 @@ class Deploy
   end
 
   def install_ruby
-    checked_run('sudo', 'apt-get', 'update')
-    checked_run('sudo', 'apt-get', 'install', '-y', 'build-essential')
-    unless valid_command?('which', 'ruby-install')
-      archive_path = '/tmp/ruby-install-0.7.0.tar.gz'
-      checked_run('wget', '-O', archive_path,
-        'https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz')
-      checked_run('tar', '-C', '/tmp','-xzvf', archive_path)
-      ruby_install_dir = '/tmp/ruby-install-0.7.0'
-      checked_run('sudo', 'make', 'install', dir: ruby_install_dir)
+    checked_run("sudo", "apt-get", "update")
+    checked_run("sudo", "apt-get", "install", "-y", "build-essential")
+    unless valid_command?("which", "ruby-install")
+      archive_path = "/tmp/ruby-install-0.7.0.tar.gz"
+      checked_run("wget", "-O", archive_path,
+                  "https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz")
+      checked_run("tar", "-C", "/tmp", "-xzvf", archive_path)
+      ruby_install_dir = "/tmp/ruby-install-0.7.0"
+      checked_run("sudo", "make", "install", dir: ruby_install_dir)
     end
-    checked_run('sudo', 'ruby-install', '-L')
-    checked_run('sudo', 'ruby-install', '--no-reinstall', '--jobs=4', 'ruby',
-      REQUIRED_RUBY_VERSION)
+    checked_run("sudo", "ruby-install", "-L")
+    checked_run("sudo", "ruby-install", "--no-reinstall", "--jobs=4", "ruby",
+                REQUIRED_RUBY_VERSION)
   end
 
   def checked_run(*args, dir: nil)
-    command = args.join(' ')
+    command = args.join(" ")
     puts "Running #{command}"
     if !dir.nil?
       command = "cd #{dir} && #{command}"
@@ -59,51 +58,50 @@ class Deploy
   end
 
   def valid_command?(*args)
-    command = args.join(' ')
+    command = args.join(" ")
     puts "Checking #{command}"
     result = @connection.exec!(command)
     result.exitstatus == 0
   end
 
   def copy_application_files
-    temp_dir = '/tmp/temp-app-dir'
-    checked_run('sudo', 'rm', '-rf', temp_dir)
-    puts 'Uploading application files'
-    @scp.upload!(File.expand_path('..', __dir__), temp_dir, recursive: true)
-    checked_run('sudo', 'mkdir', '-p', APP_DIR)
-    checked_run('sudo', 'cp', '-R', File.join(temp_dir, '*'), APP_DIR)
-    checked_run('sudo', 'rm', '-rf', temp_dir)
+    temp_dir = "/tmp/temp-app-dir"
+    checked_run("sudo", "rm", "-rf", temp_dir)
+    puts "Uploading application files"
+    @scp.upload!(File.expand_path("..", __dir__), temp_dir, recursive: true)
+    checked_run("sudo", "mkdir", "-p", APP_DIR)
+    checked_run("sudo", "cp", "-R", File.join(temp_dir, "*"), APP_DIR)
+    checked_run("sudo", "rm", "-rf", temp_dir)
   end
 
   def ruby_installation_path
-    File.expand_path("/opt/rubies/ruby-#{REQUIRED_RUBY_VERSION}/bin") 
+    File.expand_path("/opt/rubies/ruby-#{REQUIRED_RUBY_VERSION}/bin")
   end
 
   def install_required_gems(application_directory)
-    checked_run('sudo', File.join(ruby_installation_path, 'bundle'),
-      'install', '--gemfile', File.join(application_directory, 'Gemfile'),
-      '--jobs=4', '--retry=3',
-      '--without=development deployment'
-    )
+    checked_run("sudo", File.join(ruby_installation_path, "bundle"),
+                "install", "--gemfile", File.join(application_directory, "Gemfile"),
+                "--jobs=4", "--retry=3",
+                "--without=development deployment")
   end
 
   def create_app_user(user_name, application_directory)
-    unless valid_command?('id', user_name)
-      checked_run('sudo', 'useradd', user_name, '--home-dir', application_directory,
-        '-M', '-s', '/bin/bash')
+    unless valid_command?("id", user_name)
+      checked_run("sudo", "useradd", user_name, "--home-dir", application_directory,
+                  "-M", "-s", "/bin/bash")
     end
-    checked_run('sudo', 'chown', "#{user_name}:", "-R", application_directory)
+    checked_run("sudo", "chown", "#{user_name}:", "-R", application_directory)
   end
 
   def setup_systemd_service(application_directory)
-    template = File.read(File.expand_path('application.service.erb', __dir__))
+    template = File.read(File.expand_path("application.service.erb", __dir__))
     path = [
       ruby_installation_path,
-      '/usr/local/bin',
-      '/usr/bin',
-      '/bin',
-    ].join(':')
-    bundle_path = File.join(ruby_installation_path, 'bundle')
+      "/usr/local/bin",
+      "/usr/bin",
+      "/bin",
+    ].join(":")
+    bundle_path = File.join(ruby_installation_path, "bundle")
     clojure = binding
     baked_template = ERB.new(template).result(clojure)
 
@@ -111,25 +109,24 @@ class Deploy
     file_path = File.join(__dir__, service_file_name)
     File.write(file_path, baked_template)
 
-    remote_service_file = '/tmp/systemd.service'
+    remote_service_file = "/tmp/systemd.service"
     @scp.upload(file_path, remote_service_file)
-    
-    checked_run('sudo', 'mv', remote_service_file, 
-      File.join('/etc/systemd/system', "#{SERVICE_NAME}.service"))
-    checked_run('sudo', 'systemctl', 'daemon-reload')
+
+    checked_run("sudo", "mv", remote_service_file,
+                File.join("/etc/systemd/system", "#{SERVICE_NAME}.service"))
+    checked_run("sudo", "systemctl", "daemon-reload")
   end
 
   def enable_systemd_service
-    checked_run('sudo', 'systemctl', 'enable', SERVICE_NAME)
+    checked_run("sudo", "systemctl", "enable", SERVICE_NAME)
   end
 
   def restart_systemd_service
-    checked_run('sudo', 'systemctl', 'restart', SERVICE_NAME)
+    checked_run("sudo", "systemctl", "restart", SERVICE_NAME)
   end
 end
 
 if __FILE__ == $0
   deployer = Deploy.new
-  deployer.deploy('192.168.0.138', 'user', 'user')
+  deployer.deploy("192.168.0.138", "user", "user")
 end
-
